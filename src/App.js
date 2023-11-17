124bd0940f3196793e4aa060b9ad78e975948fd4
GulloMagico <33353590+gullomagico@users.noreply.github.com>
2023-11-17 22:38:49 +0100

    Prettier config and fix logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<header className="App-header">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../../assets/drbodinilogo.png';
+
 const Navbar = () => {
-  return (
-  <div>
-    <div>
-      <img src="../public/assets/drbodinilogo.png" />
-    </div>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Lavori</a></li>
-        <button class="cta-button">Contatti</button>
-      </ul>
-    </nav>
-  </div>
-  );
-}
+	return (
+		<div>
+			<div>
+				<img src={logo} alt="logo" />
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Lavori</a>
+					</li>
+					<button class="cta-button">Contatti</button>
+				</ul>
+			</nav>
+		</div>
+	);
+};
 
 export default Navbar;
